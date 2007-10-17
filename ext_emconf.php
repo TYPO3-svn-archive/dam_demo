c0e38fe96a78d8b810565c7febc1a05bad1b65f8
stucki <stucki@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-10-17 08:35:25 +0000

    Takeover by the DAM development team
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_demo/trunk@6994 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Extend DAM with own fields, indexing rules and a category tree.',
 	'category' => 'example',
 	'shy' => 0,
-	'version' => '1.0.100',
+	'version' => '1.0.101',
 	'dependencies' => 'cms,dam',
 	'conflicts' => '',
 	'priority' => '',
@@ -27,9 +27,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => 'tx_dam',
 	'clearcacheonload' => 0,
 	'lockType' => '',
-	'author' => 'Rene Fritz',
-	'author_email' => 'r.fritz@colorcube.de',
-	'author_company' => 'Colorcube - digital media lab, www.colorcube.de',
+	'author' => 'The DAM development team',
+	'author_email' => 'typo3-project-dam@lists.netfielders.de',
+	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'constraints' => array(
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"f731";s:12:"ext_icon.gif";s:4:"999b";s:17:"ext_localconf.php";s:4:"fd84";s:14:"ext_tables.php";s:4:"5a5d";s:14:"ext_tables.sql";s:4:"dd1e";s:12:"icon_cat.gif";s:4:"c6e5";s:36:"action/class.tx_damdemo_pathInfo.php";s:4:"6e9c";s:21:"action/ext_tables.php";s:4:"539a";s:28:"action/locallang_actions.xml";s:4:"1224";s:14:"doc/manual.sxw";s:4:"4b12";s:21:"fields/ext_tables.php";s:4:"ac65";s:23:"fields/locallang_db.xml";s:4:"6df2";s:40:"indexrule/class.tx_damdemo_indexrule.php";s:4:"55db";s:27:"indexrule/ext_localconf.php";s:4:"4ae6";s:34:"indexrule/locallang_indexrules.xml";s:4:"9f05";s:45:"previewer/class.tx_damdemo_previewerFlash.php";s:4:"cc50";s:24:"previewer/ext_tables.php";s:4:"bef0";s:37:"selection/class.tx_damdemo_damcat.php";s:4:"3a81";s:27:"selection/ext_localconf.php";s:4:"457b";s:28:"sv1/class.tx_damdemo_sv1.php";s:4:"0a37";s:21:"sv1/ext_localconf.php";s:4:"9ff5";s:28:"sv2/class.tx_damdemo_sv2.php";s:4:"55ac";s:21:"sv2/ext_localconf.php";s:4:"4683";}',
+	'_md5_values_when_last_written' => '',
 	'suggests' => array(
 	),
 );
